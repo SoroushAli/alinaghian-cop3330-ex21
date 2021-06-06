6644bb1fd309e828996182fb6e84565418edcaa5
SoroushAli <84461814+SoroushAli@users.noreply.github.com>
2021-06-06 08:54:36 -0400

    Initial commit

---
+++ src/main/java/base/App.java
@@ -0,0 +1,29 @@
+/*
+ *  UCF COP3330 Summer 2021 Assignment 1 Solution
+ *  Copyright 2021 Soroush Alinaghian
+ */
+package base;
+/*
+Write a program that converts a number from 1 to 12 to the corresponding month. Prompt for a number and display the
+corresponding calendar month, with 1 being January and 12 being December. For any value outside that range, display an appropriate error message.
+
+Example Output
+
+Please enter the number of the month: 3
+The name of the month is March.
+
+Constraints
+
+Use a switch or case statement for this program.
+Use a single output statement for this program.
+
+Challenges
+
+Use a map or dictionary to remove the switch statement from the program.
+Support multiple languages. Prompt for the language at the beginning of the program.
+ */
+public class App {
+    public static void main(String[] args) {
+
+    }
+}
